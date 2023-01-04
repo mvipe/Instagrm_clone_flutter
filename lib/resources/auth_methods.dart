5abd97f71d9cc3bac06546ab62a202d8366300eb
mvipe <mohnishvats@gmail.com>
2023-01-05 01:31:23 +0530

    implemented pick image from gallery

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,26 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AuthMethods{
+class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<String> signUpUser({
-    required String email,
-    required String userName,
-    required String password,
-    required String bio,
-    //required Uint8List file
-}) async{
-  String res="Some Error occured";
-  try{
-  if(email.isNotEmpty || userName.isNotEmpty || password.isNotEmpty || bio.isNotEmpty ){
-    //register user
-    UserCredential cred=await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future<String> signUpUser(
+      {required String email,
+      required String userName,
+      required String password,
+      required String bio,
+      //required Uint8List file
+      }) async {
+    String res = "Some Error occured";
+    try {
+      if (email.isNotEmpty ||
+          userName.isNotEmpty ||
+          password.isNotEmpty ||
+          bio.isNotEmpty
+          // ||file != null
+      ) {
+        //register user
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
 
-    //add user to our firestore database
-    await _firestore.collection('users').doc(cred.user!.uid).set(
-        {
+        //add user to our firestore database
+        await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': userName,
           'uid': cred.user!.uid,
           'email': email,
@@ -30,17 +35,12 @@ class AuthMethods{
           'followings': [],
         });
 
-    print(cred.user!.uid);
-
-    res="success";
-
-
+        print(cred.user!.uid);
+        res = "success";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
   }
-  }catch(err){
-    res=err.toString();
-
-
-  }
-  return res;
-}
 }
